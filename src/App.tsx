c43b2189d67745fc160d791596c2928dd6cf8679
feverdreme <jeffreyhuang08@gmail.com>
2024-02-03 16:13:34 -0500

    Add boilerplate for routes based on figma

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home";
+import TeamsPage from "./pages/teams";
 import "./App.css";
+import EventsPage from "./pages/events";
+import FAQPage from "./pages/faq";
+import SponsorsPage from "./pages/sponsors";
+import ApplyPage from "./pages/apply";
 
 function App() {
   return (
     <>
-        <main>
-          <Routes>
-            {/* <Route path="/" element={<HomePage />} />
-            <Route path="team" element={<TeamPage />} />
-            <Route path="faq" element={<FAQPage />} />
-            <Route path="events" element={<EventsPage />} />
-            <Route path="getinvolved" element={<GetInvolvedPage />} /> */}
-          </Routes>
-        </main>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="team" element={<TeamsPage />} />
+          <Route path="faq" element={<FAQPage />} />
+          <Route path="events" element={<EventsPage />} />
+          <Route path="sponsors" element={<SponsorsPage />} />
+          <Route path="apply" element={<ApplyPage />} />
+        </Routes>
+      </main>
     </>
   );
 }
