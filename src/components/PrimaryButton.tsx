9d1494843e0ec1764c96326adf07375c446f335a
feverdreme <jeffreyhuang08@gmail.com>
2024-02-06 02:16:17 -0500

    Add PrimaryButton based on Figma

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,9 @@
+import { Button, styled } from "@mui/material";
+
+const PrimaryButton = styled(Button)({
+  border: "2px black solid",
+  borderRadius: "1em",
+  boxShadow: "0.2em 0.3em 0px 0.2em black"
+})
+
+export default PrimaryButton;
