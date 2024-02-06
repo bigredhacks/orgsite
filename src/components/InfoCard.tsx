6c9dd7dd6f2b6beff9a43bf16eb39f2ee4db5b0d
feverdreme <jeffreyhuang08@gmail.com>
2024-02-06 10:15:57 -0500

    Add documentation for InfoCard

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -11,17 +11,24 @@ interface propTypes {
   children: React.ReactNode;
 }
 
+/**
+ * Basic style component taken from Figma. borderRadius is customizable
+ * with the existence of `topLeft`, `topRight`, `bottomRight`, `bottomLeft` props.
+ * 
+ * Example: <InfoCard topLeft bottomRight>{child}</InfoCard>
+ */
 export default function InfoCard(props: propTypes) {
   const styles = {
     padding: "5px",
     border: "2px black solid",
     borderRadius: [
-      (props?.topLeft ? "3em" : "0.5em"),
-      (props?.topRight ? "3em" : "0.5em"),
-      (props?.bottomRight ? "3em" : "0.5em"), 
-      (props?.bottomLeft ? "3em" : "0.5em"),
-      ].join(" "),
-    ...props.sx};
+      props?.topLeft ? "3em" : "0.5em",
+      props?.topRight ? "3em" : "0.5em",
+      props?.bottomRight ? "3em" : "0.5em",
+      props?.bottomLeft ? "3em" : "0.5em",
+    ].join(" "),
+    ...props.sx,
+  };
 
-  return <Card sx={styles}>{props.children}</Card>
+  return <Card sx={styles}>{props.children}</Card>;
 }
