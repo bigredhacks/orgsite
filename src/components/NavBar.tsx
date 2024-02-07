1e2a455ff2492a4fc08004987752b2de0d1df1f4
feverdreme <jeffreyhuang08@gmail.com>
2024-02-07 15:04:49 -0500

    Add NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,54 @@
+import { Box, Typography, Button } from "@mui/material";
+
 export default function NavBar() {
-  return <></>
+  return (
+    <Box
+      id="navbar-wrapper"
+      sx={{
+        width: "100%",
+        position: "absolute",
+        top: "0px",
+        right: "0px",
+        display: "flex",
+      }}
+    >
+      <Box sx={{ flex: "1" }}>
+        <img
+          src="/MLH.png"
+          style={{ width: "7rem", position: "absolute", left: "3rem" }}
+        />
+      </Box>
+      <Box
+        id="navbar-links"
+        sx={{
+          padding: "2rem",
+          display: "flex",
+          justifyContent: "end",
+        }}
+      >
+        <Button variant="secondary">
+          <Typography>Teams</Typography>
+        </Button>
+        <Button variant="secondary">
+          <Typography>Events</Typography>
+        </Button>
+        <Button variant="secondary">
+          <Typography>FAQ</Typography>
+        </Button>
+        <Button variant="secondary">
+          <Typography>Sponsors</Typography>
+        </Button>
+
+        <Button
+          variant="primary"
+          sx={{
+            backgroundColor: "#FF3225",
+            px: "1rem",
+          }}
+        >
+          <Typography>Apply</Typography>
+        </Button>
+      </Box>
+    </Box>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
+import { ThemeProvider } from '@mui/material';
+import theme from "./theme"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
