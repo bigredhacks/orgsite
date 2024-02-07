1476a0cb5a43d0a24369d9907a883956b614bdcc
feverdreme <jeffreyhuang08@gmail.com>
2024-02-07 14:16:15 -0500

    Fix text color and hover in styled Box

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,9 +1,13 @@
 import { Button, styled } from "@mui/material";
 
 const PrimaryButton = styled(Button)({
+  color: "black",
   border: "2px black solid",
   borderRadius: "1em",
-  boxShadow: "0.2em 0.3em 0px 0.2em black"
-})
+  boxShadow: "0.2em 0.3em 0px 0.2em black",
+  "&:hover": {
+    border: "2px black solid"
+  },
+});
 
 export default PrimaryButton;
