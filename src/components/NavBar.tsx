8e9dc4842b25cf12bd63a8bf42dd89b013c24c92
feverdreme <jeffreyhuang08@gmail.com>
2024-02-07 15:12:33 -0500

    Add links to NavBar items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
@@ -26,28 +27,41 @@ export default function NavBar() {
           justifyContent: "end",
         }}
       >
-        <Button variant="secondary">
-          <Typography>Teams</Typography>
-        </Button>
-        <Button variant="secondary">
-          <Typography>Events</Typography>
-        </Button>
-        <Button variant="secondary">
-          <Typography>FAQ</Typography>
-        </Button>
-        <Button variant="secondary">
-          <Typography>Sponsors</Typography>
-        </Button>
+        <Link to="/teams">
+          <Button variant="secondary" sx={{ height: "100%" }}>
+            <Typography>Teams</Typography>
+          </Button>
+        </Link>
 
-        <Button
-          variant="primary"
-          sx={{
-            backgroundColor: "#FF3225",
-            px: "1rem",
-          }}
-        >
-          <Typography>Apply</Typography>
-        </Button>
+        <Link to="/events">
+          <Button variant="secondary" sx={{ height: "100%" }}>
+            <Typography>Events</Typography>
+          </Button>
+        </Link>
+
+        <Link to="/faq">
+          <Button variant="secondary" sx={{ height: "100%" }}>
+            <Typography>FAQ</Typography>
+          </Button>
+        </Link>
+
+        <Link to="/sponsors">
+          <Button variant="secondary" sx={{ height: "100%" }}>
+            <Typography>Sponsors</Typography>
+          </Button>
+        </Link>
+
+        <Link to="/apply">
+          <Button
+            variant="primary"
+            sx={{
+              backgroundColor: "#FF3225",
+              px: "1rem",
+            }}
+          >
+            <Typography>Apply</Typography>
+          </Button>
+        </Link>
       </Box>
     </Box>
   );
