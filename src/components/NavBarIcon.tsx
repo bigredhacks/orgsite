380fb019300a11a4926e4b67dcebaae0090c39c8
feverdreme <jeffreyhuang08@gmail.com>
2024-02-13 00:31:16 -0500

    Remove MLH banner

--- src/components/NavBarIcon.tsx
+++ src/components/NavBarIcon.tsx
@@ -1,30 +1,22 @@
 import { Box } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function NavBarIcon() {
-  const location = useLocation();
-
   return (
     <Box sx={{ flex: 1, textAlign: "left" }}>
-      {location.pathname == "/" ? (
-        <img
-          src="/MLH.png"
-          style={{ width: "7rem", position: "absolute", left: "3rem" }}
-        />
-      ) : (
+      <Link to="/">
         <img
           src="/brh-logo.png"
           style={{
             width: "3rem",
             height: "3rem",
             marginLeft: "2rem",
-            marginTop: "2rem",
             padding: "0.75rem",
             border: "2px solid #DDDDDD",
             borderRadius: "25%",
           }}
         />
-      )}
+      </Link>
     </Box>
   );
 }
