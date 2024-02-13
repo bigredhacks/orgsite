8ebdff14f90a27d589563434bbb5c4e5a4b38842
feverdreme <jeffreyhuang08@gmail.com>
2024-02-13 00:44:58 -0500

    Revert "Fix path routing bug"
    This reverts commit b9a8904bf66642ab8da9dc932db161570a88fb7c.

--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -1,8 +1,3 @@
-import { Typography } from "@mui/material";
-
 export default function TeamsPage() {
-  return <>
-    <Typography variant="h1">teams</Typography>
-    hello
-  </>;
+  return <></>;
 }
