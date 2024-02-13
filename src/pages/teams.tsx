b9a8904bf66642ab8da9dc932db161570a88fb7c
feverdreme <jeffreyhuang08@gmail.com>
2024-02-13 00:44:14 -0500

    Fix path routing bug

--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -1,3 +1,8 @@
+import { Typography } from "@mui/material";
+
 export default function TeamsPage() {
-  return <></>;
+  return <>
+    <Typography variant="h1">teams</Typography>
+    hello
+  </>;
 }
