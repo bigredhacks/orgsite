62a0eef886f3914e1953026e0553ee827101d0b8
feverdreme <jeffreyhuang08@gmail.com>
2024-02-14 02:19:50 -0500

    Rrefactor distribution of state

--- src/components/TeamsDropdown.tsx
+++ src/components/TeamsDropdown.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Typography } from "@mui/material";
 import InfoCard, { InfoCardProps, BorderSpec } from "./InfoCard";
-import React from "react";
+import TeamMemberList from "../components/TeamMemberList.tsx";
+import { useState } from "react";
 
 export interface TeamsDropdownProps extends InfoCardProps {
   imgSrc: string;
@@ -10,54 +11,54 @@ export interface TeamsDropdownProps extends InfoCardProps {
   borderSpecList?: BorderSpec[];
 }
 
-export interface TeamsDropdownPropsClickable extends TeamsDropdownProps {
-  memberState: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
-}
-
-export default function TeamsDropdown(props: TeamsDropdownPropsClickable) {
+export default function TeamsDropdown(props: TeamsDropdownProps) {
   const { sx, imgSrc, teamName, description, placeImgRight, borderSpecList } =
     props;
-  const [showMembers, setShowMembers] = props.memberState;
+  const [showMembers, setShowMembers] = useState(false);
 
   // Change the order of text and image
   const flexDir = placeImgRight ? "row-reverse" : "row";
 
   return (
-    <InfoCard
-      sx={{
-        display: "flex",
-        flexDirection: flexDir,
-        ...sx,
-      }}
-      borderSpecList={borderSpecList}
-    >
-      <Box sx={{ maxHeight: "fit-content" }}>
-        {/* TODO: Mobile responsiveness is wack */}
-        <img src={imgSrc} style={{ maxHeight: "150px" }}></img>
-      </Box>
-      <Box sx={{ textAlign: "left" }}>
-        <Button
-          variant="primary"
-          sx={{
-            backgroundColor: "white",
-            "&:hover": { backgroundColor: "white" },
-            width: "50%",
-            marginBottom: "1rem",
-          }}
-        >
-          <Typography>{teamName}</Typography>
-        </Button>
-        <Typography>{description}</Typography>
-        <Button
-          variant="secondary"
-          sx={{ px: "0" }}
-          onClick={() => setShowMembers(!showMembers)}
-        >
-          <Typography style={{ textDecoration: "underline" }}>
-            Click here to meet the {teamName} team &#x25BC;
-          </Typography>
-        </Button>
-      </Box>
-    </InfoCard>
+    <>
+      <InfoCard
+        sx={{
+          display: "flex",
+          flexDirection: flexDir,
+          ...sx,
+        }}
+        borderSpecList={borderSpecList}
+      >
+        <Box sx={{ maxHeight: "fit-content" }}>
+          {/* TODO: Mobile responsiveness is wack */}
+          <img src={imgSrc} style={{ maxHeight: "150px" }}></img>
+        </Box>
+        <Box sx={{ textAlign: "left" }}>
+          <Button
+            variant="primary"
+            sx={{
+              backgroundColor: "white",
+              "&:hover": { backgroundColor: "white" },
+              width: "50%",
+              marginBottom: "1rem",
+            }}
+          >
+            <Typography>{teamName}</Typography>
+          </Button>
+          <Typography>{description}</Typography>
+          <Button
+            variant="secondary"
+            sx={{ px: "0" }}
+            onClick={() => setShowMembers(!showMembers)}
+          >
+            <Typography style={{ textDecoration: "underline" }}>
+              Click here to meet the {teamName} team &#x25BC;
+            </Typography>
+          </Button>
+        </Box>
+      </InfoCard>
+
+      {showMembers ? <TeamMemberList teamName={teamName} /> : <></>}
+    </>
   );
 }
--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -1,21 +1,9 @@
 import { Box, Typography } from "@mui/material";
 import TeamsDropdown from "../components/TeamsDropdown";
-import React from "react"
 
 import teamsData from "../data/teamsData.ts";
-import TeamMemberList from "../components/TeamMemberList.tsx";
-import { useState } from "react";
 
 export default function TeamsPage() {
-  const stateManager: {
-    [key: string]: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
-  } = {};
-
-  for (const {teamName} of teamsData) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    stateManager[teamName] = useState(false);
-  }
-
   return (
     <Box sx={{ width: "80vw", mx: "auto", maxWidth: "690px" }}>
       <Typography variant="h1">teams</Typography>
@@ -27,17 +15,7 @@ export default function TeamsPage() {
 
       <Box sx={{ display: "flex", flexDirection: "column", gap: "40px" }}>
         {teamsData.map((props) => (
-          <>
-            <TeamsDropdown
-              {...props}
-              memberState={stateManager[props.teamName]}
-            />
-            {stateManager[props.teamName][0] ? (
-              <TeamMemberList teamName={props.teamName} />
-            ) : (
-              <></>
-            )}
-          </>
+          <TeamsDropdown {...props} />
         ))}
       </Box>
     </Box>
