f336503f6ccbf06762a9871a0ca7f75afc5338c5
evzhu <ejzhu8512@gmail.com>
2024-02-14 01:36:20 -0500

    Footer text, social media links, styling, and bg - formatting doesn't look quite right on some dimensions for some reason

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import FAQPage from "./pages/faq";
 import SponsorsPage from "./pages/sponsors";
 import ApplyPage from "./pages/apply";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="sponsors" element={<SponsorsPage />} />
           <Route path="apply" element={<ApplyPage />} />
         </Routes>
+        <Footer />
       </main>
     </>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,57 @@
+import { Box, Stack, Typography, IconButton } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
+
+const Footer = () => {
+  return (
+    <Box id="footer-wrapper" sx={{
+      width: "100%",
+      position: "absolute",
+      bottom: "0px",
+      display: "flex",
+      justifyContent: "center",
+      backgroundColor: "#22262C",
+    }}>
+      <Stack spacing={1} mt="10px" mb="10px">
+        <Typography variant="h6" alignSelf="center" color="white">
+          contact us!
+        </Typography>
+        <Stack sx={{ display: 'flex', justifyContent: 'center', gap: '5px', flexDirection: 'row' }}>
+          <IconButton aria-label="Email">
+            <a href="mailto:bigredhacks@cornell.edu">
+              <EmailIcon sx={{ color: 'white', fontSize: '25px' }} />
+            </a>
+          </IconButton>
+          <IconButton aria-label="Instagram">
+            <a href="https://www.instagram.com/bigredhacks/" target="_blank" rel="noopener noreferrer">
+              <InstagramIcon sx={{ color: 'white', fontSize: '25px' }} />
+            </a>
+          </IconButton>
+          <IconButton aria-label="Facebook">
+            <a href="https://www.facebook.com/bigredhacks/" target="_blank" rel="noopener noreferrer">
+              <FacebookIcon sx={{ color: 'white', fontSize: '25px' }} />
+            </a>
+          </IconButton>
+          <IconButton aria-label="X">
+            <a href="https://twitter.com/BigRedHacks" target="_blank" rel="noopener noreferrer">
+              <XIcon sx={{ color: 'white', fontSize: '25px' }} />
+            </a>
+          </IconButton>
+          <IconButton aria-label="LinkedIn">
+            <a href="https://www.linkedin.com/in/bigred-hacks-698107269" target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon sx={{ color: 'white', fontSize: '25px' }} />
+            </a>
+          </IconButton>
+        </Stack>
+        <Typography variant="h6" alignSelf="center" color="white">
+          made with 💙 by the BRH team
+        </Typography>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Footer;
