673ef892a57bb60c5f2371250ccb1882d711efef
feverdreme <jeffreyhuang08@gmail.com>
2024-02-13 23:42:12 -0500

    Fix NavBarButton props type naming

--- src/components/NavBarButton.tsx
+++ src/components/NavBarButton.tsx
@@ -2,7 +2,7 @@ import { Theme } from "@emotion/react";
 import { Button, SxProps, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
-interface propTypes {
+export interface NavBarButtonProps {
   variant: string;
   text: string;
   to?: string;
@@ -12,7 +12,7 @@ interface propTypes {
 /**
  * For use in conjunction with `NavBar` component.
  */
-export default function NavBarButton(props: propTypes) {
+export default function NavBarButton(props: NavBarButtonProps) {
   // Because typescript can't recognize variant unions correctly
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const fixVariant: any = props.variant;
