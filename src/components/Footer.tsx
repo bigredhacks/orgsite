1b56acd2657adca8cf213fe8e350c7968fae49d2
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-02-17 16:21:16 -0500

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,6 @@ const Footer = () => {
   return (
     <Box id="footer-wrapper" sx={{
       width: "100%",
-      position: "absolute",
       bottom: "0px",
       display: "flex",
       justifyContent: "center",
