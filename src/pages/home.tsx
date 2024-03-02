c7ebdbb47ec097b808f03708c260a29151d63cee
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-03-02 17:19:44 -0500

    fix countdown error

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,6 @@ import { Hero } from "../components/Hero";
 import FAQPage from "../pages/faq"
 import SponsorsPage from "../pages/sponsors"
 import EventsPage from "./events";
-import Countdown from "../components/Countdown"
 
 export default function HomePage() {
   return (
