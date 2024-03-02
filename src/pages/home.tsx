37b3aaa9fb1553dacf5c19e59a9ca997235694dd
ky322 <kellyy.aot@gmail.com>
2024-03-02 17:09:15 -0500

    home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,17 @@
 import { Hero } from "../components/Hero";
+import FAQPage from "../pages/faq"
+import SponsorsPage from "../pages/sponsors"
+import EventsPage from "./events";
+import Countdown from "../components/countdown"
 
 export default function HomePage() {
-  return <Hero></Hero>
+  return (
+    <>
+  <Hero></Hero>
+  <EventsPage></EventsPage>
+  <FAQPage></FAQPage>
+  <SponsorsPage></SponsorsPage>
+  <Countdown></Countdown>
+  </>
+  )
 }
