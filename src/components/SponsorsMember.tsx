cd443093291b865aad6947769948b92634468c17
ky322 <kellyy.aot@gmail.com>
2024-04-20 15:57:32 -0400

    sponsors

--- src/components/SponsorsMember.tsx
+++ src/components/SponsorsMember.tsx
@@ -1,19 +1,53 @@
-import { Box, Typography } from "@mui/material";
-
+import { Card, Box } from "@mui/material";
 export interface SponsorsMemberProps {
   imgSrc: string;
   name: string;
   size?: string;
+  color: string;
 }
-
 export default function SponsorsMember(props: SponsorsMemberProps) {
   const { imgSrc, name } = props;
   const size = props.size ?? "inherit";
-
+  const color = props.color;
   return (
-    <Box sx={{ maxWidth: size, textAlign: "center", margin: 5 }}>
-      <img src={imgSrc} style={{ width: "100%", height: "100%", maxHeight:"100px" }} alt={name} />
-      <Typography sx={{ mt: "5px" }}>{name}</Typography>
+    <Box sx={{
+      maxWidth: size,
+      textAlign: "center",}}>
+      <Box
+        sx={{
+          width: 250,
+          height: 120,
+          backgroundColor: "white",
+          boxSizing: "border-box",
+          border: 4,
+          borderColor: "#22262C",
+          borderBottomLeftRadius: 15,
+          borderBottomRightRadius: 15,
+          borderTopRightRadius: 15,
+          borderTopLeftRadius: 60,
+          overflow: "hidden",
+          alignItems: "center",
+          position: "relative",
+          zIndex: 500,
+        }}
+      >
+        <img src={imgSrc} style={{overflow: 'hidden', width: "80%", height: "80%", objectFit: "contain", paddingTop: 10}} alt={name} />
+      </Box>
+      <Card
+        sx={{
+          width: 250,
+          height: 110,
+          backgroundColor: color,
+          borderBottomLeftRadius: 10,
+          borderBottomRightRadius: 15,
+          borderTopRightRadius: 15,
+          borderTopLeftRadius: 60,
+          overflow: "hidden",
+          left: 2,
+          top: 13,
+          position: "absolute",
+        }}>
+      </Card>
     </Box>
   );
 }
